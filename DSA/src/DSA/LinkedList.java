2243263efca4fb77e47b2533a4886c47c5aaa4a8
Ayush Khuntia <ayushkhuntialitu@gmail.com>
2025-09-16 11:29:35 +0530

    LinkedList

---
+++ DSA/src/DSA/LinkedList.java
@@ -0,0 +1,36 @@
+package DSA;
+
+
+public class LinkedList {
+	static Node head=null;
+	public static void add(int data) {
+		Node newnode = new Node(data);
+		Node temp=head;
+		if(head==null) {
+			head=newnode;
+			return;
+		}
+		else {
+			while(temp.link!=null) {
+				temp=temp.link;
+			}
+			temp.link=newnode;
+		}
+	}
+	public static void print() {
+		Node temp=head;
+		while(temp!=null) {
+			System.out.println(temp.data);
+			temp=temp.link;
+		}
+	}
+	public static void main(String[] args){
+		add(10);
+		add(20);
+		add(30);
+		add(40);
+		print();
+		
+	}
+		
+}
