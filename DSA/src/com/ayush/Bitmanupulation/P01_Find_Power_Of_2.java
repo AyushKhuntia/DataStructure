fd4616b31a54ebb957593b3db618ed5fdb34a522
Ayush Khuntia <ayushkhuntialitu@gmail.com>
2025-11-09 08:07:39 +0530

    09.11.25

---
+++ DSA/src/com/ayush/Bitmanupulation/P01_Find_Power_Of_2.java
@@ -0,0 +1,21 @@
+package com.ayush.Bitmanupulation;
+import java.util.Scanner;
+public class P01_Find_Power_Of_2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x;
+		x = sc.nextInt();
+		// Find 2^x using left shift operator
+		System.out.println("The power of 2^x is : "+ (1<<x));
+		// Check if x is a power of 2
+		if((x & (x-1)) == 0) {
+			System.out.println(x + " is a power of 2");
+		}
+		else {
+			System.out.println(x + " is not a power of 2");
+		}
+
+	}
+
+}
